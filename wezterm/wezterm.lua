d1405178b28b2a345221f950d43fad4fb17e3df2
midnight <ivanator666@gmail.com>
2025-07-06 21:58:50 +0300

    update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,17 +1,11 @@
--- Pull in the wezterm API
+-- initialize wezterm api
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
-
--- color scheme:
 config.color_scheme = 'Gruvbox Dark (Gogh)'
-
--- opticacy
-config.window_background_opacity = 0.9
-
--- and finally, return the configuration to wezterm
+config.window_background_opacity = 0.5
 config.enable_wayland = false
+config.enable_tab_bar = false
+
+-- return config
 return config
