369d1b9f9c44eafc4d5780c8cce5a7e284562695
midnight <ivanator666@gmail.com>
2025-08-03 20:31:39 +0300

    removed some broken code

--- nvim/lua/midnight/set.lua
+++ nvim/lua/midnight/set.lua
@@ -38,10 +38,3 @@ vim.g.mapleader = " "
 vim.filetype.add({
   pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
 })
-
-local mygroup = vim.api.nvim_create_augroup("loading_netrwPlugin", {clear = true})
-vim.api.nvim_create_autocmd({"VimEnter"}, {
-    pattern = {"*"},
-    command  = ":silent! Explore",
-    group = mygroup
-})
