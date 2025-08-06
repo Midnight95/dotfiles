aea88f4b20ec91032741083f490d4a7cd5a56131
midnight95 <ivanator666@gmail.com>
2025-08-06 14:08:24 +0300

    decreased transparency

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 config.color_scheme = 'Gruvbox Dark (Gogh)'
-config.window_background_opacity = 0.6
+config.window_background_opacity = 0.8
 config.enable_wayland = false
 config.enable_tab_bar = false
 config.font = wezterm.font 'Comic Code'
